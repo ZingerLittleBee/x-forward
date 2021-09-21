22798ab15a52fe0557ebb742e2f976cb047cdb13
ZingerLittleBee <6970999@gmail.com>
2021-09-21 22:04:56 +0800

    💥 feat: add local terminal

--- packages/app/src/app.module.ts
+++ packages/app/src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { EnvModule } from './modules/env/env.module'
 import { ShellModule } from './modules/shell/shell.module'
-
+// TypeOrmModule.forRoot()
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(),
-    EnvModule,
-    ShellModule
-  ]
+    imports: [EnvModule, ShellModule]
 })
 export class AppModule {}
