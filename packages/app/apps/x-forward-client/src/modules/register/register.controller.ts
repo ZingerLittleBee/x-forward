2ebd7a20ac3e749038190276fcaec56eae4a9464
ZingerLittleBee <6970999@gmail.com>
2022-03-08 23:38:09 +0800

    style: 💄 remove hard code on endpoint

--- packages/app/apps/x-forward-client/src/modules/register/register.controller.ts
+++ packages/app/apps/x-forward-client/src/modules/register/register.controller.ts
@@ -1,13 +1,13 @@
 import { Controller } from '@nestjs/common'
 import { GrpcMethod } from '@nestjs/microservices'
-import { Result, UserProperty } from '@x-forward/common'
+import { GrpcEndPoint, Result, UserProperty } from '@x-forward/common'
 import { RegisterService } from './register.service'
 
 @Controller()
 export class RegisterController {
     constructor(private readonly registerService: RegisterService) {}
 
-    @GrpcMethod('ExecutorService')
+    @GrpcMethod(GrpcEndPoint.EXECUTOR_SERVICE)
     updatePortAndUserRelation(args: { userProperties: UserProperty[] }) {
         this.registerService.setUserProperty(args?.userProperties)
         return Result.ok()
--- packages/app/libs/executor/src/executor.contoller.ts
+++ packages/app/libs/executor/src/executor.contoller.ts
@@ -1,13 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Controller } from '@nestjs/common'
 import { GrpcMethod } from '@nestjs/microservices'
 import { Result } from '@x-forward/common'
-import { ExecutorEndPoint } from '@x-forward/common/constants/endpoint.constant'
+import { GrpcEndPoint } from '@x-forward/common/constants/endpoint.constant'
 import { ExecutorService } from '@x-forward/executor/services/executor.service'
 @Controller()
 export class ExecutorController {
     constructor(private readonly executorService: ExecutorService) {}
 
-    @GrpcMethod('ExecutorService')
+    @GrpcMethod(GrpcEndPoint.EXECUTOR_SERVICE)
     async getNginxBin() {
         return Result.okData(await this.executorService.getNginxBin())
     }
@@ -17,7 +17,7 @@ export class ExecutorController {
      * args as sting
      * @returns GrpcNginxConfig
      */
-    @GrpcMethod('ExecutorService')
+    @GrpcMethod(GrpcEndPoint.EXECUTOR_SERVICE)
     async getNginxConfigArgs() {
         const configArgs = await this.executorService.getNginxConfigArgs()
         return Result.okData({
@@ -26,33 +26,38 @@ export class ExecutorController {
         })
     }
 
-    @GrpcMethod('ExecutorService')
+    @GrpcMethod(GrpcEndPoint.EXECUTOR_SERVICE)
     async getNginxStreamConfigContent() {
         return Result.okData(await this.executorService.getNginxStreamConfigContent())
     }
 
-    @GrpcMethod('ExecutorService')
+    @GrpcMethod(GrpcEndPoint.EXECUTOR_SERVICE)
     async rewriteMainConfig(args: { content: string }) {
         return Result.okData(await this.executorService.rewriteMainConfig(args?.content))
     }
 
-    @GrpcMethod('ExecutorService')
+    @GrpcMethod(GrpcEndPoint.EXECUTOR_SERVICE)
     async fetchDirectory(args: { url: string }) {
         return Result.okData(await this.executorService.getDirByUrl(args?.url))
     }
 
-    @GrpcMethod('ExecutorService')
+    @GrpcMethod(GrpcEndPoint.EXECUTOR_SERVICE)
     async getNginxStatus() {
         return Result.okData(await this.executorService.queryNginxStatus())
     }
 
-    @GrpcMethod('ExecutorService')
+    @GrpcMethod(GrpcEndPoint.EXECUTOR_SERVICE)
     async getSystemInfo() {
         return Result.okData(await this.executorService.getSystemInfo())
     }
 
-    @Post(ExecutorEndPoint.STREAM_PATCH)
-    async streamPatch(@Body() newStreamString: string) {
-        this.executorService.patchStream(newStreamString)
+    @GrpcMethod(GrpcEndPoint.EXECUTOR_SERVICE)
+    async streamPatch(args: { content: string }) {
+        try {
+            this.executorService.patchStream(args?.content)
+        } catch (e) {
+            return Result.noWithMsg(e)
+        }
+        return Result.ok()
     }
 }
