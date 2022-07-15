ef1075a0d5ff10b9238b287bc9d1ed963f3b78c9
ZingerLittleBee <6970999@gmail.com>
2022-07-16 01:59:50 +0800

    feat: 🎸 drawer v-model

--- packages/view/src/components/Drawer/index.vue
+++ packages/view/src/components/Drawer/index.vue
@@ -1,32 +1,36 @@
 <script setup lang="ts">
-import { h, ref, VNode } from 'vue'
+import { computed, h, VNode } from 'vue'
 
 interface Props {
     side?: 'left' | 'right'
     trigger?: string | VNode
     contentClass?: string
+    modelValue?: boolean
+    teleportToBody?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
     side: 'left',
-    trigger: 'Open drawer'
+    teleportToBody: true
 })
 
-const computedSide = () => {
-    const { side } = props
-    return side === 'left' ? 'drawer' : 'drawer drawer-end'
-}
+const emit = defineEmits(['update:modelValue'])
 
-const checked = ref(false)
+const { side } = props
+const sideClass = side === 'left' ? 'drawer' : 'drawer drawer-end'
 
-const Trigger = (props: { trigger: string | VNode }) => {
+const containerClasses = computed(() => {
+    return props.modelValue ? `${sideClass} h-screen` : `${sideClass}`
+})
+
+const Trigger = (props: { trigger: string | VNode; value?: boolean }) => {
     const { trigger } = props
     if (typeof trigger === 'string') {
         return h(
             'label',
             {
                 class: 'btn btn-primary drawer-button',
-                onClick: () => (checked.value = !checked.value)
+                onClick: () => emit('update:modelValue', props.value)
             },
             trigger
         )
@@ -34,7 +38,7 @@ const Trigger = (props: { trigger: string | VNode }) => {
     return h(
         'div',
         {
-            onClick: () => (checked.value = !checked.value)
+            onClick: () => emit('update:modelValue', props.value)
         },
         h(trigger)
     )
@@ -42,23 +46,31 @@ const Trigger = (props: { trigger: string | VNode }) => {
 </script>
 
 <template>
-    <div :class="computedSide()">
-        <input id="my-drawer" type="checkbox" v-model="checked" class="drawer-toggle" />
-        <div class="drawer-content">
-            <template v-if="!$slots.trigger">
-                <Trigger :trigger="props.trigger" />
-            </template>
-            <div v-else @click="() => (checked = !checked)">
-                <slot name="trigger" />
+    <teleport to="body" :disabled="!props.teleportToBody">
+        <div class="absolute h-0 top-0 left-0 z-50" :class="containerClasses">
+            <input
+                id="my-drawer"
+                type="checkbox"
+                :checked="modelValue"
+                @change="e => emit('update:modelValue', e.target.checked)"
+                class="drawer-toggle"
+            />
+            <div class="drawer-content">
+                <template v-if="props.trigger && !$slots.trigger">
+                    <Trigger :trigger="props.trigger" :value="props.modelValue" />
+                </template>
+                <div v-else @click="() => (checked = !checked)">
+                    <slot name="trigger" />
+                </div>
+            </div>
+            <div class="drawer-side">
+                <label for="my-drawer" class="drawer-overlay"></label>
+                <div class="drawer-side__overlay" :class="contentClass">
+                    <slot />
+                </div>
             </div>
         </div>
-        <div class="drawer-side">
-            <label for="my-drawer" class="drawer-overlay"></label>
-            <div class="drawer-side__overlay" :class="contentClass">
-                <slot name="content" />
-            </div>
-        </div>
-    </div>
+    </teleport>
 </template>
 
 <style scoped>
--- packages/view/src/examples/drawer.vue
+++ packages/view/src/examples/drawer.vue
@@ -1,19 +1,29 @@
 <template>
-    <Drawer class="h-full" side="right" content-class="drawer-example__content">
-        <template #trigger>
-            <button class="btn btn-primary drawer-button">Open drawer slot</button>
-        </template>
-        <template #content>
+    <!--    <Drawer class="h-full" side="right" content-class="drawer-example__content">-->
+    <!--        <template #trigger>-->
+    <!--            <button class="btn btn-primary drawer-button">Open drawer slot</button>-->
+    <!--        </template>-->
+    <!--        <template #content>-->
+    <!--            <div class="h-full flex justify-center">-->
+    <!--                <FormExample />-->
+    <!--            </div>-->
+    <!--        </template>-->
+    <!--    </Drawer>-->
+    <Drawer v-model="checked" content-class="drawer-example__content">
+        <template #default>
             <div class="h-full flex justify-center">
                 <FormExample />
             </div>
         </template>
     </Drawer>
+    <button class="btn" @click="() => (checked = !checked)">Click Me!</button>
 </template>
 
 <script setup lang="ts">
 import Drawer from '@/components/Drawer/index.vue'
 import FormExample from '@/examples/form.vue'
+import { ref } from 'vue'
+const checked = ref(false)
 </script>
 
 <style>
